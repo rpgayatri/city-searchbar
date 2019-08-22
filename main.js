2f095f291b33c90ac9cf78834a97e91b21769b32
rpgayatri <rpgayatri@gmail.com>
2019-08-22 22:52:06 +1000

    pushing code for simple searchbar

---
+++ main.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+  const endpoint =
+    "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+  const cities = [];
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data => cities.push(...data));
+
+  const textInputField = document.querySelector(".city-search");
+  const searchResults = document.querySelector(".search-results");
+
+  function searchForCity() {
+    const searchInput = textInputField.value;
+
+    if (searchInput) {
+      const filteredCities = cities.filter(cityObj =>
+        cityObj.city.includes(searchInput)
+      );
+      var cityList = filteredCities
+        .map(cityObj => `<li class="result-item">${cityObj.city}</li>`)
+        .join("");
+
+      searchResults.innerHTML = cityList;
+    } else {
+      searchResults.innerHTML = "";
+    }
+  }
+
+  textInputField.addEventListener("keyup", searchForCity);
+};
